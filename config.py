cdf049752fc075bbc3f99d40780a9a847c5c447c
fixxx <160222479+fi-xxx@users.noreply.github.com>
2025-07-25 23:52:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 # WeatherAPI
-WEATHER_API_KEY = "30cb0cbf8be64460a7f6d1e3fc11ed7f"
-WEATHER_API_HOST = "p93fbxkbwt.re.qweatherapi.com"
+WEATHER_API_KEY = "xxxxxxxxxxxxxxx"
+WEATHER_API_HOST = "xxxxxxxxxxxxxxxxxxxx"
 WEATHER_CONST_PATH = "/v7/weather/3d"
-LOCATION_CODE = "101210810"
+LOCATION_CODE = "asdas"
 
 # NewsAPI
 FUNNY_SENTENCE_URRL = "https://v2.xxapi.cn/api/dog"
 
 # 邮箱配置
-EMAIL_SENDER = "1526279668@qq.com"
+EMAIL_SENDER = "xxxxxxxxxxxxxcom"
 SMTP_PORT = 465
 SMTP_SERVER = "smtp.qq.com"
-EMAIL_PASS = "tqobyyegmxukiibc"
-EMAIL_RECEIVER = "2756787649@qq.com"
+EMAIL_PASS = "xxxxxxxxxxxxxx"
+EMAIL_RECEIVER = "asdasdasdsacom"
